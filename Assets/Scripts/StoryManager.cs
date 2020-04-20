3743cab91309fb170aa7510f3b27c1cfa892e729
blingblin-g <luvbliny@gmail.com>
2020-04-20 23:14:18 +0900

    add StoryManager

---
+++ Assets/Scripts/StoryManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManager : MonoBehaviour
+{
+    [SerializeField] private DialogController dialogController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogController.ShowText("HAHA!", 1.0f, 0.02f);
+    }
+
+    public void PushButton()
+    {
+        Debug.Log("why not");
+        dialogController.ShowText("스트레스 +50", 0.5f, 0.02f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
