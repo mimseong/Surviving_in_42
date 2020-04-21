5f928505b3086a6b8314941e9c1ff82e303952d6
rockpell <aythffk@gmail.com>
2020-04-21 17:38:31 +0900

    add DailyStsory, RegisteStory

---
+++ Assets/Scripts/DailyStory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DailyStory : MonoBehaviour, Story
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FirstMent(DialogController dialogController)
+    {
+        //if (GameManager.instance.GetSchedule() == Schedule.MORNING)
+        dialogController.ShowText("상쾌한 아침이다.", 0.5f, 0.02f);
+        dialogController.ShowText("이번엔 뭘할까?", 1.0f, 0.02f);
+    }
+}
