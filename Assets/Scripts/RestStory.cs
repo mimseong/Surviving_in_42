313fe90486ccd444c197fe2d10d018c53c7992c3
blingblin-g <luvbliny@gmail.com>
2020-04-24 00:45:44 +0900

    add RestScene
    update images and ments

---
+++ Assets/Scripts/RestStory.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestStory : MonoBehaviour
+{
+    [SerializeField] private RestUIManager restUIManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DecideRest(DialogController dialogController, Work work)
+    {
+        GameManager.instance.SetWork(work);
+        switch (work)
+        {
+            case Work.DRINKING:
+                Beer(dialogController);
+                break;
+            case Work.SLEEP:
+                Sleep(dialogController);
+                break;
+            case Work.GO_HOME:
+                GoHome(dialogController);
+                break;
+            case Work.LAZY:
+                Lazy(dialogController);
+                break;
+        }
+    }
+
+    public void Beer(DialogController dialogController)
+    {
+        dialogController.ShowTexts(0.5f, 1.5f, 0.02f, "동료들과 함께 술을 마셨다", "언제까지 어깨춤을 추게 할거야~", "동료와의 관계 += 5", "만취해서 집에 갑니다");
+    }
+
+    public void Sleep(DialogController dialogController)
+    {
+        dialogController.ShowTexts(0.5f, 1.5f, 0.02f, "소파에서 조금 자기로 합니다", "...\n..\n...", "졸림 -= 30 \n스트레스 -= 10");
+    }
+
+    public void GoHome(DialogController dialogController)
+    {
+        dialogController.ShowTexts(0.5f, 1.5f, 0.02f, "이만 집에 가는 게 좋겠다", "집에서 씻고 청결도가 회복됩니다", "집에서 자고 졸림이 해소됩니다", "졸림 = 0 \n청결 = MAX \n스트레스 -= 50");
+    }
+
+    public void Lazy(DialogController dialogController)
+    {
+        int situation = Random.Range(0, 5);
+        switch (situation)
+        {
+            case 0:
+                dialogController.ShowTexts(0.5f, 1.5f, 0.02f, "도비와 함께 춤을!", "도비와 사진 찍고 놀았다");
+                break;
+            case 1:
+                dialogController.ShowTexts(0.5f, 1.5f, 0.02f, "동료들과 수다를 떨었다", "동료A : 할게 너무 많아서 힘들어 \n동료B : 그거 나는 다 맞았는데 \n동료들 : ...", "...\n...\n...");
+                break;
+            case 2:
+                dialogController.ShowTexts(0.5f, 1.5f, 0.02f, "유튜브 알고리즘을 따라 게임방송까지 흘러갔다", "내가 이걸 왜 보고 있지", "...\n...\n...", "재밌어서 계속 보게 된다");
+                break;
+            case 3:
+                dialogController.ShowTexts(0.5f, 1.5f, 0.02f, "멍하니 검은 창을 바라보았다", "...\n...\n...", "아무것도 안했는데 시간이 왜이리 잘가지?");
+                break;
+            case 4:
+                dialogController.ShowTexts(0.5f, 1.5f, 0.02f, "동료들과 노닥거렸다", "동료A : 기침은 Calloc Calloc! \n동료들 : 하하하! 너무 웃겨! \n동료B : 또 시작이군", "...\n...\n...");
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/RestUIManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RestUIManager : MonoBehaviour
+{
+    [SerializeField] private Image beer;
+    [SerializeField] private Image sleep;
+    [SerializeField] private Image goHome;
+    [SerializeField] private Image lazy;
+    [SerializeField] private Button nextButton;
+    [SerializeField] private Image terminal;
+    [SerializeField] private RestStory restStory;
+    [SerializeField] private DialogController dialogController;
+    [SerializeField] private Sprite[] drinking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.instance.SetWork(Work.LAZY);
+        ActiveImage();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActiveTerminal()
+    {
+        terminal.gameObject.SetActive(true);
+        restStory.DecideRest(dialogController, GameManager.instance.GetWork());
+    }
+
+    public void NextButton(bool value)
+    {
+        nextButton.gameObject.SetActive(value);
+    }
+
+    public void ActiveImage()
+    {
+        Work tmp = GameManager.instance.GetWork();
+        switch (tmp)
+        {
+            case Work.DRINKING:
+                RandomSetImage(beer, drinking);
+                beer.gameObject.SetActive(true);
+                break;
+            case Work.SLEEP:
+                sleep.gameObject.SetActive(true);
+                break;
+            case Work.GO_HOME:
+                goHome.gameObject.SetActive(true);
+                break;
+            case Work.LAZY:
+                break;
+        }
+        if (tmp == Work.DRINKING || tmp == Work.SLEEP || tmp == Work.GO_HOME || tmp == Work.LAZY)
+            Invoke("ActiveTerminal", 1.2f);
+    }
+
+    public void RandomSetImage(Image target, Sprite[] sprites)
+    {
+        int index = Random.Range(0, sprites.Length);
+        if (index != 0)
+            target.sprite = sprites[index];
+    }
+}
